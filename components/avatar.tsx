87c67f01a59e7ebf2c422ae590db2201c5e0000f
Hai Nguyen <ngthanhhai.vn@gmail.com>
2022-12-04 23:39:09 +0700

    responsive update

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -13,7 +13,7 @@ export default function Avatar ({src, alt, size = 70, className = '', onClick}: 
         onClick={(e) => handleClick(e)}
         width={size}
         height={size}
-        className={`inline-block avatar-image w-full ml-4 mt-10 min-[320px]:m-0 hover:scale-110 hover:border-2 rounded-full ${className}`}
+        className={`inline-block avatar-image ml-4 mt-10 min-[320px]:m-0 min-[320px]:w-28 hover:scale-110 hover:border-2 rounded-full ${className}`}
     />
     );
 };
